efcf59d4c02641d86a3a4ea4eaf8da7dc67a5142
Andy Kim <luckyjin98@gmail.com>
2020-07-24 02:35:54 +0000

    Last

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 import Layout from './hoc/Layout/Layout';
+import First from './components/First/First';
 import Introduce from './components/Introduce/Introduce';
 import Meeting from './components/Meeting/Meeting';
 import Activity from './components/Activity/Activity';
+import Login from './containers/Login/Login';
 
 function App() {
   return (
     <div className="App">
           <Layout>
               <Switch>
-                  <Route exact path="/" render={props => (<h1>Byc - 환영해요</h1>)}></Route>
+                  <Route exact path="/" component={First}></Route>
                   <Route path="/introduce" component={Introduce}></Route>
                   <Route path="/meeting" component={Meeting}></Route>
                   <Route path="/activity" component={Activity}></Route>
+                  <Route path="/login" component={Login}></Route>
               </Switch>
           </Layout>
     </div>
--- src/components/Header/NavigationItem/NavigationItem.js
+++ src/components/Header/NavigationItem/NavigationItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import './NavigationItem.scss';
 
 const NavigationItem = (props) => {
     return (
